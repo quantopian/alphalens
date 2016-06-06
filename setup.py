e147a18dcce92ce25eb6f266dd7d6cb0b489bb7c
Andrew Campbell <andrew@quantopian.com>
2016-06-06 18:16:37 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+from distutils.core import setup
+import sys
+
+long_description = ''
+
+if 'upload' in sys.argv:
+    with open('README.md') as f:
+        long_description = f.read()
+
+setup(
+    name='pyfactor',
+    version='0.0.0',
+    description='Factor analysis tools',
+    author='Quantopian Inc.',
+    author_email='andrew@quantopian.com',
+    packages=[
+        'pyfactor',
+    ],
+    long_description=long_description,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+    ],
+    url='https://github.com/quantopian/pyfactor',
+    install_requires=()
+)
