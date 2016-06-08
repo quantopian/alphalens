9e7305fabd5ba420d479bdb23fa00cc3d4cb929f
Andrew Campbell <andrew@quantopian.com>
2016-06-08 18:53:28 -0400

    tearsheet entrypoint takes factor and pricing separately

--- pyfactor/tears.py
+++ pyfactor/tears.py
@@ -1,33 +1,50 @@
-def create_factor_tear_sheet(factor_and_fp,
+import pandas as pd
+import numpy as np
+
+from utils import *
+from plotting import *
+from performance import *
+
+def create_factor_tear_sheet(factor,
+                             pricing,
+                             factor_name='factor',
+                             sector_plots=True,
                              sector_adjust=False,
                              sector_names=None,
                              days=[1, 5, 10],
                              nquantiles = 10,
                              ret_type='normal' # normal, market_excess or beta_excess
                             ):
+    start_date = factor.index.levels[0].min()
+    end_date = factor.index.levels[0].max()
 
-    adj_factor_and_fp = sector_adjust_forward_price_moves(factor_and_fp)
+
+    if sector_adjust:
+        pricing = sector_adjust_forward_price_moves(pricing)
+
+    daily_ic, _ = factor_spearman_rank_IC(factor, pricing, by_sector=False,
+                                          factor_name=factor_name)
 
     # What is the sector-netural rolling mean IC for our different forward price windows?
-    plot_daily_ic(adj_factor_and_fp, factor_name=factor_name)
+    plot_daily_ic(daily_ic)
 
     # Plot comulative returns over time for each quantile
-    plot_quantile_cumulative_return(factor_and_fp, daily_perc_ret, quantiles=nquantiles, by_quantile=False,
-                    factor_name=factor_name, days_before=days_before, days_after=days_after, std_bar=False)
+    # plot_quantile_cumulative_return(factor_and_fp, daily_perc_ret, quantiles=nquantiles, by_quantile=False,
+    #                 factor_name=factor_name, days_before=days_before, days_after=days_after, std_bar=False)
 
-    # Plot comulative returns over time, one plot for each quantile
-    plot_quantile_cumulative_return(factor_and_fp, daily_perc_ret, quantiles=nquantiles, by_quantile=True,
-                    factor_name=factor_name, days_before=days_before, days_after=days_after, std_bar=True)
+    # # Plot comulative returns over time, one plot for each quantile
+    # plot_quantile_cumulative_return(factor_and_fp, daily_perc_ret, quantiles=nquantiles, by_quantile=True,
+    #                 factor_name=factor_name, days_before=days_before, days_after=days_after, std_bar=True)
 
     # What are the sector-neutral factor decile mean returns for our different forward price windows?
-    plot_quantile_returns(adj_factor_and_fp, by_sector=False, quantiles=nquantiles, factor_name=factor_name)
+    plot_quantile_returns(factor_and_fp, by_sector=False, quantiles=nquantiles, factor_name=factor_name)
 
     # As above but more detailed, we want to know the volatility of returns
-    plot_quantile_returns_box(adj_factor_and_fp, by_sector=False, quantiles=nquantiles, factor_name=factor_name)
+    plot_quantile_returns_box(factor_and_fp, by_sector=False, quantiles=nquantiles, factor_name=factor_name)
 
     # let's have a look at the relationship between factor and returns
-    plot_factor_vs_fwdprice_distribution(adj_factor_and_fp, factor_name=factor_name)
-    plot_factor_vs_fwdprice_distribution(adj_factor_and_fp, factor_name=factor_name, remove_outliers=True)
+    # plot_factor_vs_fwdprice_distribution(factor_and_fp, factor_name=factor_name)
+    # plot_factor_vs_fwdprice_distribution(factor_and_fp, factor_name=factor_name, remove_outliers=True)
 
     # How much is the contents of the the top and bottom quintile changing each day?
     plot_top_bottom_quantile_turnover(factor, num_quantiles=5, factor_name=factor_name)
@@ -39,15 +56,17 @@ def create_factor_tear_sheet(factor_and_fp,
     # What is IC decay for each sector?
     plot_ic_by_sector(factor_and_fp, factor_name=factor_name)
 
-    if pd.to_datetime(end_date) - pd.to_datetime(start_date) > pd.Timedelta(days=70):
+    if end_date - start_date > pd.Timedelta(days=70):
         tr = 'M'
     else:
         tr = 'W'
     # What is the IC decay for each sector over time, not assuming sector neturality?
     plot_ic_by_sector_over_time(adj_factor_and_fp, time_rule=tr, factor_name=factor_name)
 
-    # What are the factor quintile returns for each sector, not assuming sector neutrality?
-    plot_quantile_returns(adj_factor_and_fp, by_sector=True, quantiles=5, factor_name=factor_name)
 
-    # As above but more detailed, we want to know the volatility of returns
-    plot_quantile_returns_box(adj_factor_and_fp, by_sector=True, quantiles=5, factor_name=factor_name)
+    if sector_plots:
+        # What are the factor quintile returns for each sector, not assuming sector neutrality?
+        plot_quantile_returns(adj_factor_and_fp, by_sector=True, quantiles=5, factor_name=factor_name)
+
+        # As above but more detailed, we want to know the volatility of returns
+        plot_quantile_returns_box(adj_factor_and_fp, by_sector=True, quantiles=5, factor_name=factor_name)
