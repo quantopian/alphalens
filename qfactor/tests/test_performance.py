38ea7034568ecad6fcaa743bf77b69850b7da6c8
Andrew Campbell <andrew@quantopian.com>
2016-06-27 12:07:33 -0400

    Remove tests for ic standard error

--- qfactor/tests/test_performance.py
+++ qfactor/tests/test_performance.py
@@ -34,8 +34,8 @@ from pandas.util.testing import (assert_frame_equal,
 
 from .. performance import (factor_information_coefficient,
                             mean_information_coefficient,
-                            quantize_factor, quantile_turnover)
-
+                            quantize_factor, quantile_turnover,
+                            factor_returns)
 
 class PerformanceTestCase(TestCase):
     dr = date_range(start='2015-1-1', end='2015-1-2')
@@ -53,60 +53,55 @@ class PerformanceTestCase(TestCase):
                             False, False,
                             dr,
                             [-1., -1.],
-                            [0., 0.]),
+                            ),
                            (factor, [1, 2, 3, 4, 4, 3, 2, 1],
                             False, False,
                             dr,
                             [1., 1.],
-                            [0., 0.]),
+                            ),
                            (factor, [1, 2, 3, 4, 4, 3, 2, 1],
                             False, True,
                             MultiIndex.from_product(
                                 [dr, [1, 2]], names=['date', 'sector']),
                             [1., 1., 1., 1.],
-                            [inf, inf, inf, inf]),
+                            ),
                            (factor, [1, 2, 3, 4, 4, 3, 2, 1],
                             True, True,
                             MultiIndex.from_product(
                                 [dr, [1, 2]], names=['date', 'sector']),
                             [1., 1., 1., 1.],
-                            [inf, inf, inf, inf])])
+                            )])
     def test_information_coefficient(self, factor, fr,
                                      sector_adjust, by_sector,
-                                     expected_ix, expected_ic_val,
-                                     expected_err_val):
+                                     expected_ix, expected_ic_val):
         fr_df = DataFrame(index=self.factor.index, columns=[1], data=fr)
 
-        ic, err = factor_information_coefficient(
+        ic = factor_information_coefficient(
             factor, fr_df, sector_adjust=sector_adjust, by_sector=by_sector)
 
         expected_ic_df = DataFrame(index=expected_ix,
                                    columns=Int64Index([1], dtype='object'),
                                    data=expected_ic_val)
 
-        expected_err_df = DataFrame(index=expected_ix,
-                                    columns=Int64Index([1], dtype='object'),
-                                    data=expected_err_val)
-
         assert_frame_equal(ic, expected_ic_df)
-        assert_frame_equal(err, expected_err_df)
+
 
     @parameterized.expand([(factor, [4, 3, 2, 1, 1, 2, 3, 4],
                             'D', False,
                             dr,
                             [-1., -1.],
-                            [0., 0.]),
+                            ),
                            (factor, [1, 2, 3, 4, 4, 3, 2, 1],
                             'W', False,
                             DatetimeIndex(['2015-01-04'],
                                           name='date', freq='W-SUN'),
                             [1.],
-                            [0.]),
+                            ),
                            (factor, [1, 2, 3, 4, 4, 3, 2, 1],
                             None, True,
                             Int64Index([1, 2], name='sector'),
                             [1., 1.],
-                            [inf, inf]),
+                            ),
                            (factor, [1, 2, 3, 4, 4, 3, 2, 1],
                             'W', True,
                             MultiIndex.from_product(
@@ -114,14 +109,13 @@ class PerformanceTestCase(TestCase):
                                                name='date', freq='W-SUN'),
                                  [1, 2]], names=['date', 'sector']),
                             [1., 1.],
-                            [inf, inf])])
+                            )])
     def test_mean_information_coefficient(self, factor, fr,
                                           by_time, by_sector,
-                                          expected_ix, expected_ic_val,
-                                          expected_err_val):
+                                          expected_ix, expected_ic_val):
         fr_df = DataFrame(index=self.factor.index, columns=[1], data=fr)
 
-        ic, err = mean_information_coefficient(
+        ic = mean_information_coefficient(
             factor, fr_df, sector_adjust=False, by_time=by_time,
             by_sector=by_sector)
 
@@ -129,12 +123,7 @@ class PerformanceTestCase(TestCase):
                                    columns=Int64Index([1], dtype='object'),
                                    data=expected_ic_val)
 
-        expected_err_df = DataFrame(index=expected_ix,
-                                    columns=Int64Index([1], dtype='object'),
-                                    data=expected_err_val)
-
         assert_frame_equal(ic, expected_ic_df)
-        assert_frame_equal(err, expected_err_df)
 
     @parameterized.expand([(factor, 4, False,
                             [1., 2., 3., 4., 4., 3., 2., 1.]),
@@ -151,6 +140,7 @@ class PerformanceTestCase(TestCase):
             index=factor.index, data=expected_vals, name='quantile')
 
         assert_series_equal(quantized_factor, expected)
+
 
     @parameterized.expand([([[1.0, 2.0, 3.0, 4.0],
                              [4.0, 3.0, 2.0, 1.0],
