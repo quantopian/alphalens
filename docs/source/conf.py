7d991c614065c9a2e389c9e1f9d3c9022a3e6107
James Christopher <jcahall@washington.edu>
2016-07-06 12:54:04 -0400

    docs build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,6 +30,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'numpydoc'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -109,7 +110,7 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
