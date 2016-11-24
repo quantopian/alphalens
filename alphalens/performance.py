Issue #107, wrong standard deviation in cumulative return plot
The standard deviation is calculated from the cumulative returns
of each asset (the totality of them, every asset for every day)
belonging to the same quantile. Actually it should be first calculated
the quantile mean cumulative returns for every "day" (period actually)
and then calculate the standard deviation of those "daily" mean returns.
In this second case the standard deviation is obviously smaller but
it is also more meaningful.

alphalens/performance.py
@@ -475,7 +475,7 @@ def average_cumulative_return_by_quantile(quantized_factor, prices,
 
     def average_cumulative_return(q_fact):
         q_returns = utils.common_start_returns(q_fact, prices,
-                             periods_before, periods_after, demeaned)
+                          periods_before, periods_after, True, demeaned)
         q_returns = q_returns.fillna(0.).add(1).cumprod() - 1
         q_returns -= q_returns.iloc[periods_before, :]
         return pd.DataFrame( {'mean': q_returns.mean(axis=1), 'std': q_returns.std(axis=1)} ).T

alphalens/utils.py
@@ -236,12 +236,13 @@ def get_clean_factor_and_forward_returns(factor,
     return factor, forward_returns
 
 
-def common_start_returns(factor, prices, before, after, demeaned):
+def common_start_returns(factor, prices, before, after, mean_by_date, demeaned):
     """
-    A date and equity pair is extracted from each index row in the factor dataframe and for each of 
-    these pairs a return series is built starting from 'before' returns before the date and ending
-    'after' returns after the date specified in the pair. All those returns series are then aligned
-    to a common index (-before to after) and returned as a single DataFrame
+    A date and equity pair is extracted from each index row in the factor
+    dataframe and for each of these pairs a return series is built starting
+    from 'before' the date and ending 'after' the date specified in the pair.
+    All those returns series are then aligned to a common index (-before to after)
+    and returned as a single DataFrame
 
     Parameters
     ----------
@@ -256,6 +257,9 @@ def common_start_returns(factor, prices, before, after, demeaned):
         How many returns to load before factor date
     after:
         How many returns to load after factor date
+    mean_by_date : bool
+        If True, compute mean returns for each date and return that
+        instead of a return series for each asset
     demeaned : bool, optional
         Compute demeaned mean returns (long short portfolio)            
     Returns
@@ -287,6 +291,8 @@ def common_start_returns(factor, prices, before, after, demeaned):
         series = returns.ix[starting_index:ending_index, equities]
         series.index = range(starting_index - day_zero_index,
                              ending_index - day_zero_index)
+        if mean_by_date:
+            series = series.mean(axis=1)
 
         all_returns.append(series)
 