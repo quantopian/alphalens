86059ad30c2367560c73bbcd0b38a83eb3f9abe8
Luca Scarabello <luca.scarabello@gmail.com>
2016-11-25 12:44:15 +0100

    Added test case for utils.common_start_returns

--- alphalens/tests/test_utils.py
+++ alphalens/tests/test_utils.py
@@ -30,7 +30,8 @@ from pandas.util.testing import (assert_frame_equal,
                                  assert_series_equal)
 
 from .. utils import (compute_forward_returns,
-                      demean_forward_returns)
+                      demean_forward_returns,
+                      common_start_returns)
 
 
 class UtilsTestCase(TestCase):
@@ -49,3 +50,54 @@ class UtilsTestCase(TestCase):
         expected[2] = [1., 0., nan, nan, nan, nan]
 
         assert_frame_equal(fp, expected)
+
+    @parameterized.expand([(2, 4, False, False,
+                            [[0.277778, 0.701250],[0.301075, 0.701552],[0.307292, 0.691290],
+                            [0.307292, 0.691290],[0.284946, 0.690879],[0.277778, 0.701250],
+                            [0.310345, 0.690479]]),
+                           (4, 2, False, True,
+                            [[0., 0.],[0., 0.],[0., 0.], [0., 0.],[0., 0.],[0., 0.],[0., 0.]]),
+                           (3, 3, True, False,
+                            [[0.310345, 0.699666],[0.277778, 0.710261],[0.301075, 0.710268],
+                             [0.307292, 0.699602],[0.307292, 0.699602],[0.284946, 0.699462],
+                             [0.277778, 0.710261]]),
+                           (1, 5, True, True,
+                            [[0., 0.],[0., 0.],[0., 0.], [0., 0.],[0., 0.],[0., 0.],[0., 0.]]),
+                          ])
+    def test_common_start_returns(self, before, after, mean_by_date, demeaned,
+                                  expected_vals):
+        dr = date_range(start='2015-1-1', end='2015-2-2')
+        dr.name = 'date'
+        tickers = ['A', 'B', 'C', 'D']
+        prices = DataFrame(index=dr, columns=tickers,
+                           data=[[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3],
+                                 [1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3],
+                                 [1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3],
+                                 [1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3],
+                                 [1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3],
+                                 [1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3],
+                                 [1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3],
+                                 [1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3],
+                                 [1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3],
+                                 [1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3],
+                                 [1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3]])
+        factor = DataFrame(index=dr, columns=tickers,
+                           data=[[1, 2, 4, 3], [1, 2, 4, 3], [1, 2, 4, 3],
+                                 [1, 2, 4, 3], [1, 2, 4, 3], [1, 2, 4, 3],
+                                 [1, 2, 4, 3], [1, 2, 4, 3], [1, 2, 4, 3],
+                                 [1, 2, 4, 3], [1, 2, 4, 3], [1, 2, 4, 3],
+                                 [1, 2, 4, 3], [1, 2, 4, 3], [1, 2, 4, 3],
+                                 [1, 2, 4, 3], [1, 2, 4, 3], [1, 2, 4, 3],
+                                 [1, 2, 4, 3], [1, 2, 4, 3], [1, 2, 4, 3],
+                                 [1, 2, 4, 3], [1, 2, 4, 3], [1, 2, 4, 3],
+                                 [1, 2, 4, 3], [1, 2, 4, 3], [1, 2, 4, 3],
+                                 [1, 2, 4, 3], [1, 2, 4, 3], [1, 2, 4, 3],
+                                 [1, 2, 4, 3], [1, 2, 4, 3], [1, 2, 4, 3]]).stack()
+        factor.index = factor.index.set_names(['date', 'asset'])
+        factor.name = 'factor'
+
+        cmrt = common_start_returns(factor, prices, before, after, mean_by_date, demeaned)
+        cmrt = DataFrame( {'mean': cmrt.mean(axis=1), 'std': cmrt.std(axis=1)} )
+        expected = DataFrame(index=range(-before, after+1), columns=['mean', 'std'], data=expected_vals)
+        assert_frame_equal(cmrt, expected)
+
