BUG: demeaning in average_cumulative_return_by_quantile
demeaning option returns wrong values if the factor asset universe
varies from date to date. The tests don't cover this scenario, so
this patch fix the issue and add a test case too.

alphalens/tests/test_utils.py
@@ -117,7 +117,8 @@ class UtilsTestCase(TestCase):
         factor.index = factor.index.set_names(['date', 'asset'])
         factor.name = 'factor'
 
-        cmrt = common_start_returns(factor, prices, before, after, False, mean_by_date, demeaned)
+        cmrt = common_start_returns(factor, prices, before, after, False, mean_by_date,
+                                    factor if demeaned else None)
         cmrt = DataFrame( {'mean': cmrt.mean(axis=1), 'std': cmrt.std(axis=1)} )
         expected = DataFrame(index=range(-before, after+1), columns=['mean', 'std'], data=expected_vals)
         assert_frame_equal(cmrt, expected)