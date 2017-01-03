BUG: demeaning in average_cumulative_return_by_quantile (#118)
* BUG: demeaning in average_cumulative_return_by_quantile

demeaning option returns wrong values if the factor asset universe
varies from date to date. The tests don't cover this scenario, so
this patch fix the issue and add a test case too.

* minor fixes

alphalens/performance.py
@@ -473,7 +473,8 @@ def average_cumulative_return_by_quantile(quantized_factor, prices,
     Parameters
     ----------
     quantized_factor : pd.Series
-        Factor quantiles indexed by date and asset.
+        Factor quantiles indexed by date and asset and
+        optional a custom group.
     prices : pd.DataFrame
         A wide form Pandas DataFrame indexed by date with assets
         in the columns. Pricing data should span the factor
@@ -492,13 +493,11 @@ def average_cumulative_return_by_quantile(quantized_factor, prices,
     -periods_before to periods_after
     """
 
-    assets = list(set(quantized_factor.index.get_level_values('asset')))
-    prices = prices[assets]
-
     def average_cumulative_return(q_fact):
+        demean = quantized_factor if demeaned else None
         q_returns = utils.common_start_returns(q_fact, prices,
                                                periods_before, periods_after,
-                                               True, True, demeaned)
+                                               True, True, demean)
         return pd.DataFrame( {'mean': q_returns.mean(axis=1), 'std': q_returns.std(axis=1)} ).T
 
     return quantized_factor.groupby(quantized_factor).apply(average_cumulative_return)

alphalens/utils.py
@@ -236,8 +236,8 @@ def get_clean_factor_and_forward_returns(factor,
     return factor, forward_returns
 
 
-def common_start_returns(factor, prices, before, after, cumulative,
-                         mean_by_date, demeaned):
+def common_start_returns(factor, prices, before, after,
+                         cumulative=False, mean_by_date=False, demean=None):
     """
     A date and equity pair is extracted from each index row in the factor
     dataframe and for each of these pairs a return series is built starting
@@ -258,13 +258,16 @@ def common_start_returns(factor, prices, before, after, cumulative,
         How many returns to load before factor date
     after:
         How many returns to load after factor date
-    cumulative: bool
+    cumulative: bool, optional
         Return cumulative returns
-    mean_by_date : bool
+    mean_by_date: bool, optional
         If True, compute mean returns for each date and return that
         instead of a return series for each asset
-    demeaned : bool, optional
-        Compute demeaned mean returns (long short portfolio)            
+    demean: pd.DataFrame, optional
+        DataFrame with at least date and equity as index, the columns are irrelevant
+        For each date a list of equities is extracted from 'demean' index and used 
+        as universe to compute demeaned mean returns (long short portfolio)
+    
     Returns
     -------
     aligned_returns : pd.DataFrame
@@ -294,11 +297,13 @@ def common_start_returns(factor, prices, before, after, cumulative,
                              ending_index - day_zero_index)
 
         if cumulative:
-            series = series.fillna(0.).add(1).cumprod()
+            series = series.add(1).cumprod()
             series = (series / series.loc[0, :]) - 1
 
-        if demeaned:
-            series = series.sub(series.mean(axis=1), axis=0)
+        if demean is not None:
+            demean_equities = demean[timestamp].index.get_level_values('asset')
+            mean   = series[demean_equities].mean(axis=1)
+            series = series.sub(mean, axis=0)
 
         series = series.loc[:, equities]
 