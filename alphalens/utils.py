475493f16d69d2d72b908cbf8547a1b2a43b304e
James Christopher <jcahall@washington.edu>
2017-02-23 15:16:40 -0800

    added deprecated warnings for old API (#144)

--- alphalens/utils.py
+++ alphalens/utils.py
@@ -15,11 +15,41 @@
 
 import pandas as pd
 import numpy as np
+import warnings
+from functools import wraps
 from IPython.display import display
 
 
 class NonMatchingTimezoneError(Exception):
     pass
+
+
+def deprecated(msg=None, stacklevel=2):
+    """
+    Used to mark a function as deprecated.
+    Parameters
+    ----------
+    msg : str
+        The message to display in the deprecation warning.
+    stacklevel : int
+        How far up the stack the warning needs to go, before
+        showing the relevant calling lines.
+    Usage
+    -----
+    @deprecated(msg='function_a is deprecated! Use function_b instead.')
+    def function_a(*args, **kwargs):
+    """
+    def deprecated_dec(fn):
+        @wraps(fn)
+        def wrapper(*args, **kwargs):
+            warnings.warn(
+                msg or "Function %s is deprecated." % fn.__name__,
+                category=DeprecationWarning,
+                stacklevel=stacklevel
+            )
+            return fn(*args, **kwargs)
+        return wrapper
+    return deprecated_dec
 
 
 def quantize_factor(factor_data, quantiles=5, bins=None, by_group=False):
