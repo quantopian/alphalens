24129647454ab9568641b4a14d77239405f82637
James Christopher <jcahall@washington.edu>
2017-07-17 12:02:55 -0700

    remove old tearsheet and deprecation warnings

--- alphalens/utils.py
+++ alphalens/utils.py
@@ -24,34 +24,6 @@ class NonMatchingTimezoneError(Exception):
     pass
 
 
-def deprecated(msg=None, stacklevel=2):
-    """
-    Used to mark a function as deprecated.
-    Parameters
-    ----------
-    msg : str
-        The message to display in the deprecation warning.
-    stacklevel : int
-        How far up the stack the warning needs to go, before
-        showing the relevant calling lines.
-    Usage
-    -----
-    @deprecated(msg='function_a is deprecated! Use function_b instead.')
-    def function_a(*args, **kwargs):
-    """
-    def deprecated_dec(fn):
-        @wraps(fn)
-        def wrapper(*args, **kwargs):
-            warnings.warn(
-                msg or "Function %s is deprecated." % fn.__name__,
-                category=DeprecationWarning,
-                stacklevel=stacklevel
-            )
-            return fn(*args, **kwargs)
-        return wrapper
-    return deprecated_dec
-
-
 def non_unique_bin_edges_error(func):
     """
     Give user a more informative error in case it is not possible
