d040cbeaca81ba3f5a8103ce35904ed60e58c3e7
George Ho <georgh0021@gmail.com>
2017-08-04 15:22:49 -0400

    STY pep8 for tests

--- alphalens/tests/test_performance.py
+++ alphalens/tests/test_performance.py
@@ -16,14 +16,11 @@
 from __future__ import division
 from unittest import TestCase
 from nose_parameterized import parameterized
-from numpy import (nan, inf, array)
+from numpy import nan
 from pandas import (
     Series,
     DataFrame,
     date_range,
-    datetime,
-    Panel,
-    Index,
     MultiIndex,
     Int64Index,
     DatetimeIndex
@@ -37,12 +34,10 @@ from .. performance import (factor_information_coefficient,
                             quantile_turnover,
                             factor_rank_autocorrelation,
                             factor_returns, factor_alpha_beta,
-                            mean_return_by_quantile,
                             average_cumulative_return_by_quantile)
 
 from .. utils import (get_forward_returns_columns,
-                      get_clean_factor_and_forward_returns,
-                      quantize_factor)
+                      get_clean_factor_and_forward_returns)
 
 
 class PerformanceTestCase(TestCase):
@@ -161,7 +156,6 @@ class PerformanceTestCase(TestCase):
 
         assert_frame_equal(ic, expected_ic_df)
 
-
     @parameterized.expand([([[1.0, 2.0, 3.0, 4.0],
                              [4.0, 3.0, 2.0, 1.0],
                              [1.0, 2.0, 3.0, 4.0],
@@ -202,7 +196,6 @@ class PerformanceTestCase(TestCase):
 
         assert_series_equal(to, expected)
 
-
     @parameterized.expand([([1, 2, 3, 4, 4, 3, 2, 1],
                             [4, 3, 2, 1, 1, 2, 3, 4],
                             False,
@@ -238,16 +231,17 @@ class PerformanceTestCase(TestCase):
                                           long_short=True,
                                           group_neutral=group_neutral)
 
-        expected = DataFrame(index=self.dr,
-                             data=expected_vals,
-                             columns=get_forward_returns_columns(factor_data.columns))
+        expected = DataFrame(
+            index=self.dr,
+            data=expected_vals,
+            columns=get_forward_returns_columns(
+                factor_data.columns))
 
         assert_frame_equal(factor_returns_s, expected)
 
-
     @parameterized.expand([([1, 2, 3, 4, 1, 1, 1, 1],
                             -1,
-                            5./6.)])
+                            5. / 6.)])
     def test_factor_alpha_beta(self, fwd_return_vals, alpha, beta):
 
         factor_data = self.factor_data.copy()
@@ -261,33 +255,32 @@ class PerformanceTestCase(TestCase):
 
         assert_frame_equal(ab, expected)
 
-
     @parameterized.expand([([[1.0, 2.0, 3.0, 4.0],
-                            [1.0, 2.0, 3.0, 4.0],
-                            [1.0, 2.0, 3.0, 4.0],
-                            [1.0, 2.0, 3.0, 4.0]],
+                             [1.0, 2.0, 3.0, 4.0],
+                             [1.0, 2.0, 3.0, 4.0],
+                             [1.0, 2.0, 3.0, 4.0]],
                             '2015-1-4',
                             1,
                             [nan, 1.0, 1.0, 1.0]),
                            ([[4.0, 3.0, 2.0, 1.0],
-                            [1.0, 2.0, 3.0, 4.0],
-                            [4.0, 3.0, 2.0, 1.0],
-                            [1.0, 2.0, 3.0, 4.0]],
+                             [1.0, 2.0, 3.0, 4.0],
+                             [4.0, 3.0, 2.0, 1.0],
+                             [1.0, 2.0, 3.0, 4.0]],
                             '2015-1-4',
                             1,
-                           [nan, -1.0, -1.0, -1.0]),
+                            [nan, -1.0, -1.0, -1.0]),
                            ([[1.0, 2.0, 3.0, 4.0],
-                            [2.0, 1.0, 4.0, 3.0],
-                            [4.0, 3.0, 2.0, 1.0],
-                            [1.0, 2.0, 3.0, 4.0],
-                            [2.0, 1.0, 4.0, 3.0],
-                            [4.0, 3.0, 2.0, 1.0],
-                            [2.0, 1.0, 4.0, 3.0],
-                            [4.0, 3.0, 2.0, 1.0],
-                            [1.0, 2.0, 3.0, 4.0],
-                            [2.0, 1.0, 4.0, 3.0],
-                            [2.0, 1.0, 4.0, 3.0],
-                            [4.0, 3.0, 2.0, 1.0]],
+                             [2.0, 1.0, 4.0, 3.0],
+                             [4.0, 3.0, 2.0, 1.0],
+                             [1.0, 2.0, 3.0, 4.0],
+                             [2.0, 1.0, 4.0, 3.0],
+                             [4.0, 3.0, 2.0, 1.0],
+                             [2.0, 1.0, 4.0, 3.0],
+                             [4.0, 3.0, 2.0, 1.0],
+                             [1.0, 2.0, 3.0, 4.0],
+                             [2.0, 1.0, 4.0, 3.0],
+                             [2.0, 1.0, 4.0, 3.0],
+                             [4.0, 3.0, 2.0, 1.0]],
                             '2015-1-12',
                             3,
                             [nan, nan, nan, 1.0, 1.0,
@@ -317,52 +310,55 @@ class PerformanceTestCase(TestCase):
         assert_series_equal(fa, expected)
 
     @parameterized.expand([(1, 2, False, 4,
-                            [[ 1.00,  0.0,  -0.50,  -0.75],
-                             [ 0.0,  0.0,  0.0,  0.0],
-                             [ 0.00,  0.00,  0.00,  0.00],
-                             [ 0.0,  0.0,  0.0,  0.0],
-                             [ -0.20,  0.0,  0.25,  0.5625],
-                             [ 0.0,  0.0,  0.0,  0.0],
-                             [ -0.3333333,  0.0,  0.50,  1.25],
-                             [ 0.0,  0.0,  0.0,  0.0]]),
+                            [[1.00, 0.0, -0.50, -0.75],
+                             [0.0, 0.0, 0.0, 0.0],
+                             [0.00, 0.00, 0.00, 0.00],
+                             [0.0, 0.0, 0.0, 0.0],
+                             [-0.20, 0.0, 0.25, 0.5625],
+                             [0.0, 0.0, 0.0, 0.0],
+                             [-0.3333333, 0.0, 0.50, 1.25],
+                             [0.0, 0.0, 0.0, 0.0]]),
                            (1, 2, True, 4,
-                            [[ 0.8833333,  0.0, -0.5625, -1.015625 ],
-                             [ 0.0,  0.0,  0.0,  0.0],
-                             [-0.1166667,  0.0, -0.0625, -0.265625],
-                             [ 0.0,  0.0,  0.0,  0.0],
-                             [-0.3166667,  0.0,  0.1875,  0.296875],
-                             [ 0.0,  0.0,  0.0,  0.0],
-                             [-0.4500000,  0.0,  0.4375,  0.984375],
-                             [ 0.0,  0.0,  0.0,  0.0]]),
+                            [[0.8833333, 0.0, -0.5625, -1.015625],
+                             [0.0, 0.0, 0.0, 0.0],
+                             [-0.1166667, 0.0, -0.0625, -0.265625],
+                             [0.0, 0.0, 0.0, 0.0],
+                             [-0.3166667, 0.0, 0.1875, 0.296875],
+                             [0.0, 0.0, 0.0, 0.0],
+                             [-0.4500000, 0.0, 0.4375, 0.984375],
+                             [0.0, 0.0, 0.0, 0.0]]),
                            (3, 0, False, 4,
-                            [[ 7.0,  3.0,  1.0,  0.0],
-                             [ 0.0,  0.0,  0.0,  0.0],
-                             [ 0.0,  0.0,  0.0,  0.0],
-                             [ 0.0,  0.0,  0.0,  0.0],
-                             [-0.488, -0.36, -0.2,  0.0],
-                             [ 0.0,  0.0,  0.0,  0.0],
-                             [-0.703704, -0.55555555, -0.333333333,  0.0],
-                             [ 0.0,  0.0,  0.0,  0.0]]),
+                            [[7.0, 3.0, 1.0, 0.0],
+                             [0.0, 0.0, 0.0, 0.0],
+                             [0.0, 0.0, 0.0, 0.0],
+                             [0.0, 0.0, 0.0, 0.0],
+                             [-0.488, -0.36, -0.2, 0.0],
+                             [0.0, 0.0, 0.0, 0.0],
+                             [-0.703704, -0.55555555, -0.333333333, 0.0],
+                             [0.0, 0.0, 0.0, 0.0]]),
                            (0, 3, True, 4,
-                            [[ 0.0, -0.5625, -1.015625, -1.488281],
-                             [ 0.0,  0.0,  0.0,  0.0],
-                             [ 0.0, -0.0625, -0.265625, -0.613281],
-                             [ 0.0,  0.0,  0.0,  0.0],
-                             [ 0.0, 0.1875, 0.296875, 0.339844],
-                             [ 0.0,  0.0,  0.0,  0.0],
-                             [ 0.0, 0.4375, 0.984375, 1.761719],
-                             [ 0.0,  0.0,  0.0,  0.0]]),
+                            [[0.0, -0.5625, -1.015625, -1.488281],
+                             [0.0, 0.0, 0.0, 0.0],
+                             [0.0, -0.0625, -0.265625, -0.613281],
+                             [0.0, 0.0, 0.0, 0.0],
+                             [0.0, 0.1875, 0.296875, 0.339844],
+                             [0.0, 0.0, 0.0, 0.0],
+                             [0.0, 0.4375, 0.984375, 1.761719],
+                             [0.0, 0.0, 0.0, 0.0]]),
                            (3, 3, False, 2,
-                            [[ 3.5,  1.5,  0.5,  0.0, -0.25, -0.375,  -0.4375],
-                             [ 0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0],
-                             [-0.595852, -0.457778, -0.266667,  0.0,  0.375,  0.90625,   1.664062],
-                             [ 0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0]]),
+                            [[3.5, 1.5, 0.5, 0.0, -0.25, -0.375, -0.4375],
+                             [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                             [-0.595852, -0.457778, -0.266667, 0.0, 0.375,
+                              0.90625, 1.664062],
+                             [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]]),
                            (3, 3, True, 2,
-                            [[ 2.047926,  0.978888,  0.383333,  0.0, -0.3125, -0.640625, -1.050781],
-                             [ 0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0],
-                             [-2.047926, -0.978888, -0.383333,  0.0,  0.3125,  0.640625,  1.050781],
-                             [ 0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0]]),
-                          ])
+                            [[2.047926, 0.978888, 0.383333, 0.0, -0.3125,
+                              -0.640625, -1.050781],
+                             [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                             [-2.047926, -0.978888, -0.383333, 0.0, 0.3125,
+                              0.640625, 1.050781],
+                             [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]]),
+                           ])
     def test_average_cumulative_return_by_quantile(self, before, after,
                                                    demeaned, quantiles,
                                                    expected_vals):
@@ -370,65 +366,67 @@ class PerformanceTestCase(TestCase):
         dr.name = 'date'
         tickers = ['A', 'B', 'C', 'D']
         r1, r2, r3, r4 = (1.25, 1.50, 1.00, 0.50)
-        data=[[r1**i,  r2**i,  r3**i,  r4**i] for i in range(1, 19)]
+        data = [[r1**i, r2**i, r3**i, r4**i] for i in range(1, 19)]
         prices = DataFrame(index=dr, columns=tickers, data=data)
         dr2 = date_range(start='2015-1-21', end='2015-1-26')
         dr2.name = 'date'
-        factor = DataFrame(index=dr2, columns=tickers,
-                           data=[[3, 4, 2, 1], [3, 4, 2, 1], [3, 4, 2, 1],
-                                 [3, 4, 2, 1], [3, 4, 2, 1], [3, 4, 2, 1]]).stack()
+        factor = DataFrame(
+            index=dr2, columns=tickers, data=[
+                [
+                    3, 4, 2, 1], [
+                    3, 4, 2, 1], [
+                    3, 4, 2, 1], [
+                        3, 4, 2, 1], [
+                            3, 4, 2, 1], [
+                                3, 4, 2, 1]]).stack()
 
-        factor_data = get_clean_factor_and_forward_returns(factor,
-                                                           prices,
-                                                           quantiles=quantiles,
-                                                           periods=range(0,after+1),
-                                                           filter_zscore=False)
+        factor_data = get_clean_factor_and_forward_returns(
+            factor, prices, quantiles=quantiles, periods=range(
+                0, after + 1), filter_zscore=False)
 
-        avgrt = average_cumulative_return_by_quantile(factor_data['factor_quantile'],
-                                                      prices,
-                                                      before,
-                                                      after,
-                                                      demeaned)
+        avgrt = average_cumulative_return_by_quantile(
+            factor_data['factor_quantile'], prices, before, after, demeaned)
         arrays = []
-        for q in range(1, quantiles+1):
+        for q in range(1, quantiles + 1):
             arrays.append((q, 'mean'))
             arrays.append((q, 'std'))
         index = MultiIndex.from_tuples(arrays, names=['factor_quantile', None])
-        expected = DataFrame(index=index, columns=range(-before,after+1), data=expected_vals)
+        expected = DataFrame(
+            index=index, columns=range(-before, after + 1), data=expected_vals)
         assert_frame_equal(avgrt, expected)
 
     @parameterized.expand([(0, 2, False, 4,
-                            [[ 0.0,  -0.50, -0.75],
-                             [ 0.0,   0.0,    0.0],
-                             [ 0.0,   0.0,    0.0],
-                             [ 0.0,   0.0,    0.0],
-                             [ 0.0,   0.25,   0.5625],
-                             [ 0.0,   0.0,    0.0],
-                             [ 0.0,   0.50,   1.25],
-                             [ 0.0,   0.0,    0.0]]),
+                            [[0.0, -0.50, -0.75],
+                             [0.0, 0.0, 0.0],
+                             [0.0, 0.0, 0.0],
+                             [0.0, 0.0, 0.0],
+                             [0.0, 0.25, 0.5625],
+                             [0.0, 0.0, 0.0],
+                             [0.0, 0.50, 1.25],
+                             [0.0, 0.0, 0.0]]),
                            (0, 3, True, 4,
-                            [[ 0.0, -0.5625, -1.015625, -1.488281],
-                             [ 0.0,  0.0,     0.0,       0.0],
-                             [ 0.0, -0.0625, -0.265625, -0.613281],
-                             [ 0.0,  0.0,     0.0,       0.0],
-                             [ 0.0,  0.1875,  0.296875,  0.339844],
-                             [ 0.0,  0.0,     0.0,       0.0],
-                             [ 0.0,  0.4375,  0.984375,  1.761719],
-                             [ 0.0,  0.0,     0.0,       0.0]]),
+                            [[0.0, -0.5625, -1.015625, -1.488281],
+                             [0.0, 0.0, 0.0, 0.0],
+                             [0.0, -0.0625, -0.265625, -0.613281],
+                             [0.0, 0.0, 0.0, 0.0],
+                             [0.0, 0.1875, 0.296875, 0.339844],
+                             [0.0, 0.0, 0.0, 0.0],
+                             [0.0, 0.4375, 0.984375, 1.761719],
+                             [0.0, 0.0, 0.0, 0.0]]),
                            (0, 3, False, 2,
-                            [[ 0.0, -0.25, -0.375,  -0.4375],
-                             [ 0.0,  0.0,  0.0,  0.0],
-                             [ 0.0,  0.375,  0.90625,   1.664062],
-                             [ 0.0,  0.0,  0.0,  0.0]]),
+                            [[0.0, -0.25, -0.375, -0.4375],
+                             [0.0, 0.0, 0.0, 0.0],
+                             [0.0, 0.375, 0.90625, 1.664062],
+                             [0.0, 0.0, 0.0, 0.0]]),
                            (0, 3, True, 2,
-                            [[ 0.0, -0.3125, -0.640625, -1.050781],
-                             [ 0.0,  0.0,  0.0,  0.0],
-                             [ 0.0,  0.3125,  0.640625,  1.050781],
-                             [ 0.0,  0.0,  0.0,  0.0]]),
-                          ])
+                            [[0.0, -0.3125, -0.640625, -1.050781],
+                             [0.0, 0.0, 0.0, 0.0],
+                             [0.0, 0.3125, 0.640625, 1.050781],
+                             [0.0, 0.0, 0.0, 0.0]]),
+                           ])
     def test_average_cumulative_return_by_quantile_2(self, before, after,
-                                                   demeaned, quantiles,
-                                                   expected_vals):
+                                                     demeaned, quantiles,
+                                                     expected_vals):
         """
         Test varying factor asset universe: at different dates there might be
         different assets
@@ -437,29 +435,28 @@ class PerformanceTestCase(TestCase):
         dr.name = 'date'
         tickers = ['A', 'B', 'C', 'D', 'E', 'F']
         r1, r2, r3, r4 = (1.25, 1.50, 1.00, 0.50)
-        data=[[r1**i,  r2**i,  r3**i,  r4**i,  r2**i,  r3**i] for i in range(1, 12)]
+        data = [[r1**i, r2**i, r3**i, r4**i, r2**i, r3**i]
+                for i in range(1, 12)]
         prices = DataFrame(index=dr, columns=tickers, data=data)
         dr2 = date_range(start='2015-1-18', end='2015-1-21')
         dr2.name = 'date'
         factor = DataFrame(index=dr2, columns=tickers,
-                           data=[[3, 4, 2, 1, nan, nan], [3, 4, 2, 1, nan, nan],
-                                 [3, nan, nan, 1, 4, 2], [3, nan, nan, 1, 4, 2]]).stack()
+                           data=[[3, 4, 2, 1, nan, nan],
+                                 [3, 4, 2, 1, nan, nan],
+                                 [3, nan, nan, 1, 4, 2],
+                                 [3, nan, nan, 1, 4, 2]]).stack()
 
-        factor_data = get_clean_factor_and_forward_returns(factor,
-                                                           prices,
-                                                           quantiles=quantiles,
-                                                           periods=range(0,after+1),
-                                                           filter_zscore=False)
+        factor_data = get_clean_factor_and_forward_returns(
+            factor, prices, quantiles=quantiles, periods=range(
+                0, after + 1), filter_zscore=False)
 
-        avgrt = average_cumulative_return_by_quantile(factor_data['factor_quantile'],
-                                                      prices,
-                                                      before,
-                                                      after,
-                                                      demeaned)
+        avgrt = average_cumulative_return_by_quantile(
+            factor_data['factor_quantile'], prices, before, after, demeaned)
         arrays = []
-        for q in range(1, quantiles+1):
+        for q in range(1, quantiles + 1):
             arrays.append((q, 'mean'))
             arrays.append((q, 'std'))
         index = MultiIndex.from_tuples(arrays, names=['factor_quantile', None])
-        expected = DataFrame(index=index, columns=range(-before,after+1), data=expected_vals)
+        expected = DataFrame(
+            index=index, columns=range(-before, after + 1), data=expected_vals)
         assert_frame_equal(avgrt, expected)
